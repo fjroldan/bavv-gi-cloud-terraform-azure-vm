f8b82ce47d46f431920f7b298abc032a3cd2b76c
Fabian Jose Roldan Pineros <fjroldan@akenfactory.com>
2023-01-30 13:24:30 -0500

    Add pipeline

--- providers.tf
+++ providers.tf
@@ -1,18 +1,27 @@
-terraform {
-  required_version = ">=0.12"
+//terraform {
+//  required_version = ">=0.12"
 
+//  required_providers {
+//    azurerm = {
+//      source  = "hashicorp/azurerm"
+//      version = "~>2.0"
+//    }
+//    random = {
+//      source  = "hashicorp/random"
+//      version = "~>3.0"
+//    }
+//    tls = {
+//      source  = "hashicorp/tls"
+//      version = "~>4.0"
+//    }
+//  }
+//}
+
+terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~>4.0"
+      version = "=3.0.0"
     }
   }
 }

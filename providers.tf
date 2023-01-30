2b4e8f6297be37cce51b31c8636ea594f218182b
Fabian Jose Roldan Pineros <fjroldan@akenfactory.com>
2023-01-30 14:01:19 -0500

    Add pipeline

--- providers.tf
+++ providers.tf
@@ -31,6 +31,6 @@ provider "azurerm" {
 
   subscription_id = "bcb520ca-0fe0-4eaa-9afc-da74b5c01bd7"
   client_id       = "aeeafcad-0b41-4f28-8c72-129641a4d94b"
-  client_secret   = "900a8b4b-f710-4a58-ab43-f52309d9996e"
+  client_secret   = "oIw8Q~oEc13kxK0aJ8OUMs~cd2~OjktsDwWxKcxd"
   tenant_id       = "1acfa458-2c17-44e5-b9d8-ff851b6c7c5f"
 }

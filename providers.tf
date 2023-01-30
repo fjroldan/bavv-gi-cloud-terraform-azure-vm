7288c88402d7d3d7e84fe4af0aa544c0fdc4fda9
Fabian Jose Roldan Pineros <fjroldan@akenfactory.com>
2023-01-30 13:35:27 -0500

    Add pipeline

--- providers.tf
+++ providers.tf
@@ -28,4 +28,9 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  subscription_id = "bcb520ca-0fe0-4eaa-9afc-da74b5c01bd7"
+  client_id       = "aeeafcad-0b41-4f28-8c72-129641a4d94b"
+  client_secret   = "900a8b4b-f710-4a58-ab43-f52309d9996e"
+  tenant_id       = "1acfa458-2c17-44e5-b9d8-ff851b6c7c5f"
 }
